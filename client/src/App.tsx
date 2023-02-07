d1fd977c49953734969bfee850c3975f1f551b31
Tomas Polivka <t_pol@pop-os.localdomain>
2023-02-07 21:00:39 +0100

    Added example FlatsOverviewTable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { FlatsOverviewTable } from "./components/app/flatsOverview/flatsTable";
+
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <FlatsOverviewTable />
+    </div>
+  );
 };
 
 export default App;
